119dcd6456cb52781125948d1e4b36d0f214541e
Fangang <fangang2004@126.com>
2024-01-19 21:21:39 +0800

    disable the TokenFilter

--- edev-trade-gateway/src/main/java/com/edev/trade/gateway/filter/TokenFilter.java
+++ edev-trade-gateway/src/main/java/com/edev/trade/gateway/filter/TokenFilter.java
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
-@Component
+//@Component
 public class TokenFilter implements GlobalFilter, Ordered {
 
     @Override
