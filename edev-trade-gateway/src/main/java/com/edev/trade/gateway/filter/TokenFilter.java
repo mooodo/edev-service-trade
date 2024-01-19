a854e5f299349c9cbaa30c33615981fda35af2fb
Fangang <fangang2004@126.com>
2024-01-20 01:33:14 +0800

    disable the TokenFilter

--- edev-trade-gateway/src/main/java/com/edev/trade/gateway/filter/TokenFilter.java
+++ edev-trade-gateway/src/main/java/com/edev/trade/gateway/filter/TokenFilter.java
@@ -17,7 +17,7 @@ public class TokenFilter implements GlobalFilter, Ordered {
     public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
         ServerHttpRequest request = exchange.getRequest();
         String token = request.getHeaders().getFirst("token");
-        if(token==null||token.equals("")) return unauthorized(exchange);
+        if(token==null|| token.isEmpty()) return unauthorized(exchange);
         //valid whether the token has authority.
         return chain.filter(exchange);
     }
